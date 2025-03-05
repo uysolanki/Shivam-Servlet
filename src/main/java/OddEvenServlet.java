4d1c64272b3b005e4adc734e746dd21321114ccf
Umar Solanki <uysolanki@gmail.com>
2025-03-05 11:19:39 +0530

    servlet day 1

---
+++ src/main/java/OddEvenServlet.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class OddEvenServlet
+ */
+@WebServlet("/OddEvenServlet")
+public class OddEvenServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public OddEvenServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		int n1= Integer.parseInt(request.getParameter("t1"));
+				if(n1%2==0)
+					out.print("<h1><font color='red'>Number is EVEN</font></h1");
+				else
+					out.print("<font color='green'>Number is ODD</font>");
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
